dee33aa05b28ce89434ce27494c71264c82604cd
Mark Grushevski <markgrushevski@gmail.com>
2021-06-26 23:12:36 +0500

    Added error logic in fetch movies

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -13,7 +13,11 @@ export default class Main extends Component {
     this.setState({loading: true});
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&language=${lang}`)
       .then(r => r.json())
-      .then(data => this.setState({movies: data.results, loading: false}));
+      .then(data => this.setState({movies: data.results, loading: false}))
+      .catch((e) => {
+        console.error(e);
+        this.setState({loading: false});
+      });
   };
   
   componentDidMount() {
