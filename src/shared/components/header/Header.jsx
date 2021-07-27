84bb064e92d9c7e3fb3c3d73a931816b6a310cb9
Mark Grushevski <markgrushevski@gmail.com>
2021-07-27 09:50:13 +0500

    Added logo constant

--- src/shared/components/header/Header.jsx
+++ src/shared/components/header/Header.jsx
@@ -1,9 +1,11 @@
+import {LOGO} from "../../constants/titles";
+
 export const Header = () => {
     return (
         <header className="header">
             <nav className="nav">
                 <div className="nav-wrapper container ">
-                    <a href="/" className="brand-logo left black-text"><b>LOGO</b></a>
+                    <a href="/" className="brand-logo left black-text"><b>{LOGO}</b></a>
                 </div>
             </nav>
         </header>
